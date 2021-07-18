7e20cb81898f74f8b748ae1d9fc1baf7fa3eb3ae
papa0601 <rapy0601@naver.com>
2021-07-18 13:53:46 +0900

    Test Commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Java Said \"Hello World\"\nAnd I Said \"Fuck You\"");
+        //System.out.println("Java Said \"Hello World\"\nAnd I Said \"Fuck You\"");
+
+        byte num = 0;
+        while (num < 100) {
+            try {
+                num++;
+                Thread.sleep(125);
+                System.out.println("당신은 " + num + "번 시도했다.");
+            } catch (InterruptedException Ignore) {Ignore.printStackTrace();
+
+            }
+        }
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n당신은 " + num + "번의 시도끝에 마침내 해냈다.");
     }
 }
