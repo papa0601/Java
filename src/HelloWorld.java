64cf0827d06910fae2e8ceab526b4024b9c03df1
papa0601 <rapy0601@naver.com>
2021-07-19 12:14:45 +0900

    Best Commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        //System.out.println("Java Said \"Hello World\"\nAnd I Said \"Fuck You\"");
-
-        byte num = 0;
-        while (num < 100) {
-            try {
-                num++;
-                Thread.sleep(125);
-                System.out.println("당신은 " + num + "번 시도했다.");
-            } catch (InterruptedException Ignore) {Ignore.printStackTrace();
-
-            }
+        String[] OList = {"3.1", "3.11", "NT", "95", "98", "2000", "ME", "XP", "Vista", "7", "8", "8.1", "10", "11"};
+        Scanner Counter = new Scanner(System.in);
+        System.out.print("리스트를 불러올 범위를 지정해주세요(1~" + OList.length + ")>>>");
+        int Count = Counter.nextInt() - 1;
+        if (Count < 0) {
+            Count = 0;
+        }else if (Count > OList.length - 1) {
+            Count = OList.length - 1;
         }
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n당신은 " + num + "번의 시도끝에 마침내 해냈다.");
+        String[] NList = new String[Count + 1];
+        for (int i = 0; i <= Count; i++) {
+            NList[i] = OList[i];
+            System.out.println(NList[i]);
+        }
+
     }
 }
