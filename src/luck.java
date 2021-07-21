86510e474add011e7d64d26da8bec6309f3f1140
papa0601 <rapy0601@naver.com>
2021-07-21 11:51:29 +0900

    Update luck.java

--- src/luck.java
+++ src/luck.java
@@ -3,14 +3,35 @@ import java.util.Scanner;
 public class luck {
     public static void main(String[] args) {
         Random rand = new Random();
-        Scanner check = new Scanner(System.in);
-        System.out.print("랜덤으로 지정할 수의 최댓값을 입력해주세요.0~(수가 클수록 확률이 낮아집니다)>>>>");
-        int max = check.nextInt();
-        int goal = rand.nextInt(max);
-        int cur = 10;
+        int max;
+
+        while (true) {
+            Scanner check = new Scanner(System.in);
+
+
+            try {
+                while (true) {
+                    System.out.print("숫자를 입력해주세요. 10~(수가 클수록 확률이 낮아집니다)>>>>");
+                    max = check.nextInt();
+                    if (max > 9) {
+                        break;
+                    }
+                    System.out.println("\n\n\n\n\n");
+                    System.out.println("10이상의 수를 입력해주세요.");
+
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("\n\n\n\n\n");
+                System.out.println("정수를 입력해주세요.");
+            }
+        }
+        int goal = rand.nextInt(max) + 10;
+        int cur = 1;
         int att = 0;
+
         while (cur != goal) {
-            cur = rand.nextInt(max) + 1;
+            cur = rand.nextInt(goal) + 1;
             att++;
             System.out.println(att + "번째 시도 : 실패");
         }
