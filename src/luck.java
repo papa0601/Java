2a84dbc20608a85987d5ad49075d4dccedc3e2aa
papa0601 <rapy0601@naver.com>
2021-07-21 11:04:13 +0900

    Hot Fix

--- src/luck.java
+++ src/luck.java
@@ -4,16 +4,17 @@ public class luck {
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner check = new Scanner(System.in);
-        System.out.println("랜덤으로 지정할 수의 최댓값을 입력해주세요.0~(수가 클수록 확률이 낮아집니다)>>>>");
+        System.out.print("랜덤으로 지정할 수의 최댓값을 입력해주세요.0~(수가 클수록 확률이 낮아집니다)>>>>");
         int max = check.nextInt();
         int goal = rand.nextInt(max);
         int cur = 10;
         int att = 0;
-        while (cur == goal) {
+        while (cur != goal) {
             cur = rand.nextInt(max) + 1;
             att++;
-            System.out.println(att + "번째 시도는 실패했습니다.");
+            System.out.println(att + "번째 시도 : 실패");
         }
-        System.out.println(att + "번째 시도가 성공했습니다!");
+        att++;
+        System.out.println(att + "번째 시도 : 성공!");
     }
 }
